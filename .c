d44dd82305372aafb9eeade9b94902df62f2ff16
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-01-26 13:50:31 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ printf("input");
+ scanf("%d",&a);
+ if(a==0)
+ {
+  printf("zero")
+ }
+ else if(a>=1)
+ {
+  printf("positive");
+ }
+ else
+ {
+  printf("negative");
+  }
+  }
