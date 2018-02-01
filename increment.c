8e126afbe7a408426d8e022fc13ef7ec3ab4a4e1
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-01 16:57:33 +0530

    Create increment.c

---
+++ increment.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter a number:");
+    scanf("%d",&a);
+    for(int i=0;i<a;i++)
+    {
+        b=(a+i);}
+    
+    printf("%d",b);
+    
+}
+
+
