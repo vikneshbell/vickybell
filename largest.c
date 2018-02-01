ee0c65d842e2b3eb3d9fde93e1cfe0b5607ea10b
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-01 15:57:11 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,26 @@
+
+
+#include<stdio.h>
+void main()
+{
+ int a,b,c;
+ scanf("%d",&a);
+ scanf("%d",&b);
+ scanf("%d",&c);
+ if(a<b)
+ {if(a<c)
+   {
+    printf("%d",a);
+   }
+   else
+   {}
+ } 
+   
+  else if(b<c)
+   {
+       printf("%d",c);
+   }
+       else("%d",b);
+     }
+     
+
