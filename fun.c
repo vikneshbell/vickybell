b4d89a8f2592592a042834a249f2c912a48fee67
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-04 10:30:11 +0530

    Create fun.c

---
+++ fun.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{  
+    int a,b,c;
+    scanf("%d%d",&a&b);
+    add(a,b);
+    sub(a,b);
+    
+}
+int add(x,y,z)
+{
+    z=x+y;
+    printf("%d",z);
+}
+int sub(q,w,e)
+{
+    e=q-w;
+    printf("%d",e);
+}
