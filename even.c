4c8a87b858b587bafc9281912bd63baac4f9e7a8
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-09 16:15:44 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+  void main()
+{
+    int a,b;
+    printf("enter a number");
+    scanf("%d%d",&a,&b);
+    for(a;a<b;a++)
+    {
+        if(a%2==0)
+        {
+            printf("%d",a);
+        }
+    }
+    
+}
