bb4b2df4178a53dd99e759b777676726764d57ea
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-02-09 16:30:05 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,a,b,i,count;
+printf("enter the two limits");
+scanf("%d%d",&a,&b);
+for(i=a;i<b;i++)
+{
+count=0;
+for(n=2;n<i;n++)
+{
+if(i%n==0)
+count++;
+}
+if(count==0)
+printf("%d\n",i);
+}
+}
