4668fd7a5d8259c8a4e0348a30818886ac3be1e4
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-03-15 15:49:42 +0530

    Create string2.c

---
+++ string2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[20];
+    int s,d=1,f;
+    printf("enter the string:");
+    gets(a);
+    f=strlen(a);
+    for(s=0;s<=f;s++)
+    {
+        if(a[s]==' ')
+        {
+            d=d+1;
+        }
+    }
+    printf("the number of words:%d",d);
+}
