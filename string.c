7124b2bbf9d19003119530b1379da5b9eb92c276
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-03-16 16:11:54 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b;
+    char c[10];
+    printf("enter the string :");
+    gets(c);
+    scanf("%d",&a);
+    for(b=1;b<=a;b++)
+    {
+        printf("%s\n",c);
+    }
+    
+    
+}
