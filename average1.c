0198d95a170dab96d0e6d6ee44094da84e490cff
vikneshbell <35727881+vikneshbell@users.noreply.github.com>
2018-03-20 15:54:12 +0530

    Create average1.c

---
+++ average1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f,g;
+    printf("enter the numbers:\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    f=a+b+c+d+e;
+    g=f/5;
+    printf("the average value is %d\n",g);
+}
